dcc545b64dd3ed48c1c5a411ebf21b830d6ef84c
eltlutfi <lutfi200499@gmail.com>
2022-04-15 23:20:55 +0700

    Creating post method

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const express = require("express");
 const res = require("express/lib/response");
 const champions = require("./data");
 
-
 const app = express();
 const port = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/api/champions", (req, res) => {
     res.status(200).json(champions);
@@ -13,6 +15,25 @@ app.get("/api/champions", (req, res) => {
 app.get('/api/champions/:id', (req, res) => {
     const champion = champions.find((e) => e.id === Number(req.params.id));
     res.status(200).json(champion);
+});
+
+app.post("/api/champions", (req, res) => {
+    
+    const { club, manager, stadium } = req.body;
+    
+    const lastId = champions[champions.length - 1].id;
+    const newId = lastId + 1;
+
+    const champion = {
+        id: newId,
+        club,
+        manager,
+        stadium,
+    };
+
+    champions.push(champion);
+    
+    res.status(201).json(champion);
 });
 
 app.listen(port, () => {
